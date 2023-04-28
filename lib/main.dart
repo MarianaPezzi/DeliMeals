39032082601efbaab1af741fea00400022e5a39f
mariana.pezzi <mariana.rafaela@senairs.org.br>
2023-04-28 10:44:40 -0300

    Navegando para tela de detalhe da refeição

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vamos_cozinhar/screens/categories_screen.dart';
+import 'package:vamos_cozinhar/screens/meal_detail_screen.dart';
 import 'package:vamos_cozinhar/utils/app_routes.dart';
 
 import 'screens/categories_meals_screen.dart';
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (context) => CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (context) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (context) => MealDetailScreen(),
       },
     );
   }
